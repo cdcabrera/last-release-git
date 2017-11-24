1708a80bf266ab65ec43e1fef4e6ce9c9e501431
CD Cabrera <cabrera.code@gmail.com>
2017-11-24 08:57:39 -0500

    fix: Create initial tag if none exists

--- src.js
+++ src.js
@@ -2,23 +2,39 @@ const { execSync } = require('child_process');
 const { clean, lt } = require('semver');
 
 module.exports = (config, pluginConfig, callback) => {
-    const refs = execSync('git show-ref --tags').toString('utf-8').trim().split('\n');
+
     let latestVersion;
     let latestVersionCommitHash;
 
-    for (let i = 0; i < refs.length; i++) {
-        const ref = refs[i];
-        const [commitHash, refName] = ref.split(' ');
-        const version = clean(refName.split('/')[2]);
+    let getVersion = function() {
 
-        // version is null if not valid
-        if (version && (!latestVersion || lt(latestVersion, version))) {
-            latestVersion = version;
-            latestVersionCommitHash = commitHash;
-        }
-    }
+        let refs = execSync('git show-ref --tags').toString('utf-8').trim().split('\n');
+
+        refs.forEach(function(ref) {
+
+            let [commitHash, refName] = ref.split(' ');
+            let version = clean(refName.split('/')[2]);
+
+            // version is null if not valid
+            if (version && (!latestVersion || lt(latestVersion, version))) {
+
+                latestVersion = version;
+                latestVersionCommitHash = commitHash;
+            }
+        });
+    };
+
+    getVersion();
 
     if (!latestVersion) {
+
+        execSync('git tag v0.0.0 && git push --tags');
+    }
+
+    getVersion();
+
+    if (!latestVersion) {
+
         throw Error('There is no valid semver git tag. Create the first valid tag via "git tag v0.0.0" and then push it via "git push --tags".');
     }
 
